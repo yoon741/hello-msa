aa2a6c76784d3693f9602ee6297ac1f643b148ac
yoon741 <mono74140@gmail.com>
2024-10-16 16:59:17 +0900

    product service - 상품 상세 조회 기능 추가, 존재 하지 않는 상품 조회 시 응답코드 404 처리 추가

--- msa-product-sevice/routes/product.py
+++ msa-product-sevice/routes/product.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Depends
+from typing import Optional
+
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from schema.product import Product, ProductBase, ProductList
 from service.database import get_db
-from service.product import register, productlist
+from service.product import register, productlist, productone
 
 router = APIRouter()
 
@@ -18,3 +20,14 @@ async def list_products(db:Session=Depends(get_db)):
     products = productlist(db)
 
     return [ProductList.model_validate(p) for p in products]
+
+@router.get('/product/{pno}', response_model=Optional[Product])
+async def product_one(pno:int, db:Session=Depends(get_db)):
+    product = productone(db,pno)
+
+    # 상품이 조회되지 않을 경우 응답코드 404를 프론트엔드로 전달
+    # 데이터가 존재하지 않는다면 오류가 아니므로 500번이 아닌 다른 예외처리를 줌
+    if product is None:
+        raise HTTPException(404, 'Product not found')
+
+    return Product.model_validate(product)
--- msa-product-sevice/service/product.py
+++ msa-product-sevice/service/product.py
@@ -19,3 +19,7 @@ def register(db: Session, product: ProductBase): # Product라는 형식으로 ro
 def productlist(db: Session):
     return db.query(Product.pno, Product.name, Product.price, Product.regdate)\
                                                     .order_by(Product.pno.desc()).all()
+
+# 상품 상세 조회
+def productone(db: Session, pno: int):
+    return db.query(Product).filter(Product.pno == pno).first()
